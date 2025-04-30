d9cc06b2e857b8bc238bfb9b11b049d7e58e8c31
Kerim_Lex <lexmeins1@gmail.com>
2025-04-30 23:59:56 +0500

    Repositories

--- example_app/lib/api.dart
+++ example_app/lib/api.dart
@@ -18,7 +18,24 @@ class GithubInfo {
   }
 }
 
-Future<GithubInfo> fetchGithubInfo(String user) async {
+class GithubRepoInfo {
+  String repoName;
+  String? description;
+  String? language;
+
+
+  GithubRepoInfo(this.repoName, this.description, this.language);
+
+  factory GithubRepoInfo.fromJson(Map<String, dynamic> json){
+    return GithubRepoInfo(
+      json['name'] as String,
+      json['description'] as String?,
+      json['language'] as String?,
+    );
+  }
+}
+
+Future<GithubInfo> fetchGithubGeneralInfo(String user) async {
   final response = await http.get(
     Uri.parse('https://api.github.com/users/$user')
   );
@@ -26,6 +43,19 @@ Future<GithubInfo> fetchGithubInfo(String user) async {
   if (response.statusCode == 200){
     return GithubInfo.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
   } else {
+    throw Exception('Failed to get account');
+  }
+}
+
+Future<List<GithubRepoInfo>> fetchGithubRepoInfo(String user) async {
+  final response = await http.get(
+    Uri.parse('https://api.github.com/users/$user/repos')
+  );
+
+  if (response.statusCode == 200){
+    final List<dynamic> jsonList = jsonDecode(response.body);
+    return jsonList.map((json) => GithubRepoInfo.fromJson(json as Map<String, dynamic>)).toList();
+  } else{
     throw Exception('Failed to get account');
   }
 }
